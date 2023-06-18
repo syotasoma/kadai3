5304f849340bd9d759071f1a98bbc7e2a8c49b84
EC2 Default User <ec2-user@ip-172-31-35-22.ec2.internal>
2023-06-18 14:00:54 +0000

    2回目

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -2,7 +2,26 @@ class BooksController < ApplicationController
   def new
     @book = Book.new
   end
+  
+  def create
+    @book = Book.new(book_image_params)
+    @book.user_id = current_image.id
+    @book.save
+    redirect_to books_path
+  end 
+  
+  def index
+    @book = Book.all
+  end 
+  
 
-  def view
+  def show
+    #12章のとこ
   end
+  
+  private
+  
+  def book_params
+    params.require(:books).paermit(:tilte, :opinion)
+  end 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
-  devise_for :users
-  resources :books, only: [:new, :index, :show]
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: "homes#top"
+  devise_for :users
+  resources :books, only: [:new,  :create, :index, :show]
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get "/homes/about" => "homes#about", as: "about"
 end
